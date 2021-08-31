e4034884501f622d4565e6e8e10612962f707f3f
Colgan Meanor <colganmeanor7@gmail.com>
2021-08-30 23:16:32 -0500

    Complete unicorn tests.

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,21 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
     this.color = color;
+  }
+  }
+  isWhite() {
+     if (this.color === 'white'){
+       return true;
+     } else {
+       return false
+     }
+  }
+  says(saying) {
+    return `**;* ${saying} *;**`
   }
 }
 
