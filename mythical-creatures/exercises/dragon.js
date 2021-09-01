f23dd6f3d912b6b228ee3ad32f94036dbc1296b7
Colgan Meanor <colganmeanor7@gmail.com>
2021-08-31 19:46:29 -0500

    Pass dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,30 @@
 
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.meals = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+      if (this.hungry && this.meals < 2){
+        this.meals++;
+      } else {
+        this.hungry = false
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+module.exports = Dragon;
