4705166a1c8894cc29c01edf83103ce81d078862
Colgan Meanor <colganmeanor7@gmail.com>
2021-09-07 20:24:17 -0500

    Complete Medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,30 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    var victim = new Statue(victim.name);
+    if (this.statues.length < 3) {
+      this.statues.push(victim)
+    } else if (this.statues.length >= 3) {
+      var freedPerson = new Person(this.statues[0].name, 'relieved')
+      this.statues.shift();
+      this.statues.push(victim);
+    return freedPerson
+    }
+  }
+  // gazeAtVictim(victim){
+  //   var victim = new Statue(victim.name);
+  //   this.statues.push(victim);
+  //   if (this.statues.length >=3){
+  //     this.statues.shift();
+  //   }
+  // }
+}
+
+module.exports = Medusa
