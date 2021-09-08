8ec5e1eb841c46b43630e0cad906b0e5e1a72f9d
Colgan Meanor <colganmeanor7@gmail.com>
2021-09-07 20:24:51 -0500

    Complete Snowman Test

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,11 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+  constructor(name, materials){
+    this.name = name,
+    this.wantsToBuildASnowman = true,
+    this.materials = {}
+  }
 }
 
 module.exports = Human;
