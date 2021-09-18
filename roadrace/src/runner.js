5385515b89d5af374de5cee982bebed410330086
Colgan Meanor <colganmeanor7@gmail.com>
2021-09-18 14:36:27 -0500

    Complete roadrace

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,22 @@
 class Runner {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+  runSomeMiles(num){
+    this.milesRun = this.milesRun + num;
+    this.fitness = this.fitness + num;
+  }
+  stretch(){
+    this.fitness = this.fitness + .5;
+  }
+  runRace(race, miles){
+      this.completedRaces.push(race);
+      this.runSomeMiles(miles);
+  }
 }
 
 module.exports = Runner;
--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,18 @@
 class VendingMachine {
-
+  constructor(machine) {
+    this.id = machine.id;
+    this.isBroken = machine.isBroken;
+    this.snacks = [];
+  }
+  addSnacks(snack) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i] !== snack) {
+        this.snacks.push(snack)
+      } else {
+        return 'Sorry, that snack is already stocked! Try adding a different snack.'
+      }
+    }
+  }
 }
 
 module.exports = VendingMachine;
